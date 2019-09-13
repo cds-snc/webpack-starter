84cc71a9d7cd540ac1097483c246f4c4260ec2d1
Tim Arney <tim@line37.com>
2019-09-13 13:53:45 -0400

    start

--- client/src/components/MyApp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Cal from './Cal'
-export const MyApp = () => {
-  return (
-    <div>
-      <Cal />
-    </div>
-  )
-}
--- config/csp.config.js
+++
@@ -1,9 +0,0 @@
-// docs: https://helmetjs.github.io/docs/csp/
-module.exports = {
-  defaultSrc: ["'self'"],
-  scriptSrc: ["'self'", "'unsafe-inline'"],
-  baseUri: ["'none'"],
-  fontSrc: ["'self'", 'https://fonts.gstatic.com'],
-  imgSrc: ["'self'", 'data:'],
-  styleSrc: ["'self'", 'https://fonts.googleapis.com'],
-}
