8c2671bff92ac94361f074ba0a5e78f49a2ef349
Tim Arney <tim@line37.com>
2019-10-18 11:42:09 -0400

    mod lookup

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const path = require("path");
 const webpack = require("webpack");
 const merge = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const FileListPlugin = require("./FileListPlugin");
 
 const getConfig = options => {
@@ -10,13 +9,12 @@ const getConfig = options => {
     plugins: [
       new webpack.ProgressPlugin(),
       new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin(options.HtmlWebpackPluginOptions),
       new FileListPlugin({ options: true })
     ],
     module: {
       rules: [
         {
-          test: /.(js|jsx)$/,
+          test: /\.(js|jsx)$/,
           include: [path.resolve(__dirname, "src")],
           loader: "babel-loader",
           options: {
